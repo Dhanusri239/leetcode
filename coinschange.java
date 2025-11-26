125911323adb4318ee034f17c44515d068b17426
Dhanusri239 <sdhanu946@gmail.com>
2025-11-26 12:18:53 +0530

    coins change code is added

---
+++ coinschange.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class coinschange {
+    public static int change(int coins[], int amount) {
+         int amt[]=new int[amount+1];
+        Arrays.fill(amt,amount+1);
+        amt[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0; j<coins.length ;j++){
+                if(i >= coins[j]){
+                    amt[i]=Math.min(amt[i] , 1+amt[i-coins[j]]);
+                }
+            }
+        }
+        if(amt[amount] < amount+1){
+            return amt[amount];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int c[] = new int[n];
+
+        for (int i = 0; i < c.length; i++) {
+            c[i] = sc.nextInt();
+        }
+        sc.nextLine();
+        int amt = sc.nextInt();
+
+        int r = change(c, amt);
+        System.out.println(r);
+    }
+}
